36cf4cab744979054132b7cfed8548d2aae280e4
laura ramgil <lauraramgil@hotmail.com>
2023-11-28 20:36:49 +0100

    Add MaintenanceTask Entity and DTO's

---
+++ src/main/java/dat3/partner/entity/MaintenancePriority.java
@@ -0,0 +1,7 @@
+package dat3.partner.entity;
+
+public enum MaintenancePriority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
---
+++ src/main/java/dat3/partner/entity/MaintenanceTask.java
@@ -0,0 +1,54 @@
+package dat3.partner.entity;
+
+import dat3.security.entity.UserWithRoles;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.text.DateFormat;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class MaintenanceTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String description;
+    private String title;
+    @Enumerated(EnumType.STRING)
+    @Column(name="status", columnDefinition = "ENUM('DONE','IN_PROGRESS','NOT_STARTED')")
+    private MaintenanceStatus status;
+    @CreationTimestamp
+    private LocalDateTime created;
+    @UpdateTimestamp
+    private LocalDateTime updated;
+    @Enumerated(EnumType.STRING)
+    @Column(name="priority", columnDefinition = "ENUM('HIGH','MEDIUM','LOW')")
+    private MaintenancePriority priority;
+    private byte[] image;
+    @ManyToOne
+    private UserWithRoles account;
+    @ManyToOne
+    private Unit unit;
+
+    public MaintenanceTask(String description, String title, MaintenanceStatus status, MaintenancePriority priority, UserWithRoles account, Unit unit){
+        this.description = description;
+        this.title = title;
+        this.status = status;
+        this.priority = priority;
+        this.account = account;
+        this.unit = unit;
+        account.addMaintenanceTask(this);
+        unit.addMaintenanceTask(this);
+    }
+
+}
