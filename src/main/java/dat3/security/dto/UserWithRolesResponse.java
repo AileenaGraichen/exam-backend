9a37cfd6586f956c5c0ca4624addd3abdee98d01
simo75m1 <simo75m1@stud.kea.dk>
2023-11-28 22:35:09 +0100

    add cleaningplan to user response

--- src/main/java/dat3/security/dto/UserWithRolesResponse.java
+++ src/main/java/dat3/security/dto/UserWithRolesResponse.java
@@ -1,6 +1,7 @@
 package dat3.security.dto;
 
 
+import dat3.partner.dto.CleaningPlanResponse;
 import lombok.Getter;
 import lombok.Setter;
 import dat3.security.entity.UserWithRoles;
@@ -15,10 +16,12 @@ public class UserWithRolesResponse {
     List<String> roleNames;
     String email;
 
+    List<CleaningPlanResponse> cleaningPlans;
     public UserWithRolesResponse(UserWithRoles userWithRoles){
         this.userName = userWithRoles.getUsername();
         this.roleNames = userWithRoles.getRoles().stream().map(role -> role.toString()).collect(Collectors.toList());
         this.email = userWithRoles.getEmail();
+        this.cleaningPlans = userWithRoles.getCleaningPlans().stream().map(plan -> new CleaningPlanResponse(plan)).toList();
     }
 
 }
