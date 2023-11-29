a7ed71da04fbfe1403a44abfc606f46568d95abb
simo75m1 <simo75m1@stud.kea.dk>
2023-11-29 13:59:00 +0100

    setup unit test file

--- src/main/java/dat3/partner/repository/CleaningPlanRepository.java
+++ src/main/java/dat3/partner/repository/CleaningPlanRepository.java
@@ -10,8 +10,12 @@ import java.util.List;
 
 @Repository
 public interface CleaningPlanRepository extends JpaRepository<CleaningPlan, Integer> {
+    boolean existsCleaningPlanByDateAndUser_Username(LocalDate date, String username);
     List<CleaningPlan> getCleaningPlansByDateAndUser_Username(LocalDate date, String username);
+
+    boolean existsCleaningPlanByUser_Username(String username);
     List<CleaningPlan> getCleaningPlansByUser_Username(String username);
+    boolean existsCleaningPlanByUnit_Id(int unitId);
     List<CleaningPlan> getCleaningPlansByUnit_Id(int unitId);
     boolean existsCleaningPlanByDateAndUnit_IdAndUser_Username(LocalDate date, int unitId, String username);
 }
