08a21711406fbd60705bceae123ffc10f046067f
Aileena <aile0074@stud.kea.dk>
2024-01-11 20:23:30 +0100

    Finish repository tests

--- src/main/java/dat3/partner/configuration/SetupDevUsers.java
+++ src/main/java/dat3/partner/configuration/SetupDevUsers.java
@@ -139,11 +139,11 @@ public class SetupDevUsers implements ApplicationRunner {
         System.out.println("**** ** ON YOUR REMOTE DATABASE                 ******************************");
         System.out.println();
         System.out.println("******************************************************************************");
-        UserWithRoles user1 = new UserWithRoles("user1", passwordUsedByAll, "user1@a.dk");
+
+        UserWithRoles user1 = new UserWithRoles("partneroe", "securepassword", "partner@kea.dk");
         UserWithRoles user2 = new UserWithRoles("user2", passwordUsedByAll, "user2@a.dk");
         UserWithRoles user3 = new UserWithRoles("user3", passwordUsedByAll, "user3@a.dk");
         UserWithRoles user4 = new UserWithRoles("user4", passwordUsedByAll, "user4@a.dk");
-        user1.addRole(Role.CLEAN);
         user1.addRole(Role.ADMIN);
         user2.addRole(Role.TECH);
         user3.addRole(Role.ADMIN);
